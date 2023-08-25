ff2053d69c58e489d4a840834975352fcd680e16
Cijo Thomas <cijo.thomas@gmail.com>
2023-08-24 17:16:20 -0700

    Add account and ns to user-event exporterdemo

--- opentelemetry-user-events-metrics/examples/basic.rs
+++ opentelemetry-user-events-metrics/examples/basic.rs
@@ -1,7 +1,9 @@
 //! run with `$ cargo run --example basic --all-features
+use std::{thread::sleep, time::Duration};
+
 use opentelemetry::{
     metrics::{MeterProvider as _, Unit},
-    KeyValue,
+    Key, KeyValue,
 };
 use opentelemetry_sdk::{
     metrics::{MeterProvider, PeriodicReader},
@@ -12,10 +14,11 @@ use opentelemetry_user_events_metrics::MetricsExporter;
 fn init_metrics(exporter: MetricsExporter) -> MeterProvider {
     let reader = PeriodicReader::builder(exporter, runtime::Tokio).build();
     MeterProvider::builder()
-        .with_resource(Resource::new(vec![KeyValue::new(
-            "service.name",
-            "metric-demo",
-        )]))
+        .with_resource(Resource::new(vec![
+            KeyValue::new("service.name", "metric-demo"),
+            KeyValue::new("_microsoft_metrics_account", "exemplardemo"),
+            KeyValue::new("_microsoft_metrics_namespace", "cithomaslinux"),
+        ]))
         .with_reader(reader)
         .build()
 }
@@ -33,21 +36,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Some(vec![KeyValue::new("key", "value")]),
     );
     let c = meter
-        .f64_counter("counter_test")
+        .f64_counter("MyFruitCounter_Rust")
         .with_description("test_decription")
         .with_unit(Unit::new("test_unit"))
         .init();
 
-    c.add(
-        1.0,
-        [
-            KeyValue::new("mykey1", "myvalue1"),
-            KeyValue::new("mykey2", "myvalue2"),
-        ]
-        .as_ref(),
-    );
+    loop
+    {
+        c.add(1.0, [ KeyValue::new("name", "apple"), KeyValue::new("color", "red"),].as_ref(),);
+        c.add(2.0, [ KeyValue::new("name", "lemon"), KeyValue::new("color", "yellow"),].as_ref(),);
+        c.add(1.0, [ KeyValue::new("name", "lemon"), KeyValue::new("color", "yellow"),].as_ref(),);
+        c.add(2.0, [ KeyValue::new("name", "apple"), KeyValue::new("color", "green"),].as_ref(),);
+        c.add(5.0, [ KeyValue::new("name", "apple"), KeyValue::new("color", "red"),].as_ref(),);
+        c.add(4.0, [ KeyValue::new("name", "lemon"), KeyValue::new("color", "yellow"),].as_ref(),);
+        
+        sleep(Duration::from_secs(1));
+        println!("Done!");
+    }
+    
 
     meter_provider.shutdown()?;
 
+    println!("Done!");
     Ok(())
 }
